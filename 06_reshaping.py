eb0e3be245f17a72e601b21d382f4318495ac57a
KuKuXia <liuxiaolongsdu@mail.sdu.edu.cn>
2019-12-31 11:03:34 +0800

    add broadcasting.py

---
+++ 06_reshaping.py
@@ -0,0 +1,67 @@
+"""
+Transformation
+"""
+
+import tensorflow as tf
+
+# Reshape
+a = tf.random.normal([4, 28, 28, 3])
+print(f'shape of a: {a.shape}, ndim of a: {a.ndim}')
+
+print(tf.reshape(a, [4, 784, 3]).shape)
+print(tf.reshape(a, [4, -1, 3]).shape)
+print(tf.reshape(a, [4, 784 * 3]).shape)
+print(tf.reshape(a, [4, -1]).shape)
+# Reshape is flexible
+print(tf.reshape(tf.reshape(a, [4, -1]), [4, 28, 28, 3]).shape)
+print(tf.reshape(tf.reshape(a, [4, -1, 3]), [-1, 3]).shape)
+# Reshape could lead to potential bugs!
+# images: [b, h, w, 3] = [ 4, 28, 28, 3] -> [b, pixel, 3]
+# 再次转换不一定就是原来的维度了，有很多可能性，如[4, 14, 56, 3]，在处理图像数据时，要记录下转换前数据的对应content，这样才能还原数据
+
+
+# 对张量数据的content进行调换
+b = tf.random.normal((4, 3, 2, 1))
+print(f'shape of b: {b.shape}')
+print(tf.transpose(b).shape)
+print(tf.transpose(b, perm=[0, 1, 3, 2]).shape)
+
+# [b, h, w, 3] -> [b, 3, h, w]
+print(tf.transpose(a, [0, 2, 1, 3]).shape)
+print(tf.transpose(a, [0, 3, 2, 1]).shape)
+print(tf.transpose(a, [0, 3, 1, 2]).shape)
+
+# Squeeze vs Expand_dims
+# Expand dim
+# c: [classes, students, classes] = [4, 25, 8]
+# add school dim
+# [1, 4, 25, 8] + [1, 4, 25, 8] = [2, 4, 25, 8]
+
+c = tf.random.normal([4, 35, 8])
+print(f'shape of c: {c.shape}')
+
+# Expand_dim： 扩展维度
+# 在最后扩展一维
+print(tf.expand_dims(c, axis=0).shape)
+print(tf.expand_dims(c, axis=-4).shape)
+
+# 在最开始扩展一维
+print(tf.expand_dims(c, axis=-1).shape)
+print(tf.expand_dims(c, axis=3).shape)
+
+# 在中间扩展一维
+print(tf.expand_dims(c, axis=2).shape)
+print(tf.expand_dims(c, axis=1).shape)
+
+# Squeeze: 将shape=1的维度删除, axis可以指定要删除的维度，前提是该维度对应的元素是1
+# [4, 35, 1, 8] -> [4, 35, 8]
+# [4, 1, 35, 1) -> [4, 35]
+
+d = tf.random.normal([1, 2, 5, 1, 1, 3, 1])
+print(f'shape of d: {d.shape}')
+print(tf.squeeze(d).shape)
+print(tf.expand_dims(d, axis=3).shape)
+print(tf.expand_dims(d, axis=3).shape)
+print(tf.squeeze(d, axis=0).shape)
+print(tf.squeeze(d, axis=-1).shape)
+print(tf.squeeze(d, axis=4).shape)
---
+++ 07_broadcasting.py
@@ -0,0 +1,29 @@
+"""
+Broadcasting: 对张量的维度进行扩展的手段，指对某一个维度重复n多次，但是并没有真正的复制数据
+tf.tile:对张量某个维度进行显式的复制n多次数据，并且会真实地在数据上体现出来
+"""
+import tensorflow as tf
+
+# Broadcasting
+a = tf.random.normal([4, 32, 32, 3])
+print(f'shape of a: {a.shape}')
+print((a + tf.random.normal([3])).shape)
+print((a + tf.random.normal([32, 32, 1])).shape)
+print((a + tf.random.normal([4, 1, 1, 1])).shape)
+# print((a + tf.random.normal([1, 4, 1, 1])).shape)  # 报错，因为第2维度有4个元素，和32不一致
+
+
+# tf.broadcast_to
+# 将张量通过broadcast变为另一个shape
+original_b = tf.random.normal([4, 1, 1, 1])
+after_b = tf.broadcast_to(original_b, [4, 32, 32, 3])
+print(f'original shape of b: {original_b.shape}, after broadcasting, b: {after_b.shape}')
+
+# tf.tile
+c = tf.ones([3, 4])
+c_1 = tf.broadcast_to(c, [2, 3, 4])
+print(f'shape of c: {c.shape}, c_1: {c_1.shape}')
+
+c_2 = tf.expand_dims(c, axis=0)
+c_3 = tf.tile(c_2, [2, 1, 1])  # 表示在特定的维度复制多少次
+print(f'shape of c_2: {c_2.shape}, c_3: {c_3.shape}')
