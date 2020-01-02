de31ebe8c3a42837c3b65bf427933b6df6ff767d
KuKuXia <liuxiaolongsdu@mail.sdu.edu.cn>
2020-01-02 19:23:14 +0800

    update test_tf2.py

--- test_tf2.py
+++ test_tf2.py
@@ -1,13 +1,8 @@
 import tensorflow as tf
 from tf_01_linear_regression import run
-from tf_12_sort_and_topk import accuracy_demo
 
 tf.random.set_seed(1234)
 
 
 def test_run():
     assert run() == (0.089, 1.478)
-
-
-def test_accuracy():
-    assert accuracy_demo() == [10.0, 20.0, 20.0, 50.0, 70.0, 100.0]
