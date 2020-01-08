d1fe0ccb6c0c8a94fc7049fb14f1d786c015e284
kukuxia <liuxiaolongsdu@gmail.com>
2020-01-08 09:50:04 +0800

    add wgan_train.py
    Signed-off-by: KuKuXia <liuxiaolongsdu@mail.sdu.edu.cn>

--- keras_gan/wgan_train.py
+++ keras_gan/wgan_train.py
@@ -1,3 +1,7 @@
+"""
+WGAN和GAN的神经网络结构是一样的，区别在于loss
+"""
+
 import glob
 import os
 
@@ -8,11 +12,14 @@ from keras_gan.dataset import make_anime_dataset
 from keras_gan.gan import Generator, Discriminator
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+gpu = tf.config.experimental.list_physical_devices(device_type='GPU')
+print(gpu)
+tf.config.experimental.set_memory_growth(device=gpu[0], enable=True)
 
 tf.random.set_seed(22)
 np.random.seed(22)
-assert tf.__version__.startswith('2.')
 
 
 def save_result(val_out, val_block_size, image_path, color_mode):
@@ -71,6 +78,29 @@ def celoss_zeros(logits):
     return tf.reduce_mean(loss)
 
 
+def gradient_penalty(discriminator, batch_x, fake_image):
+    batchsz = batch_x.shape[0]
+
+    # [b, h, w, c]
+    t = tf.random.uniform([batchsz, 1, 1, 1])
+    # [b, 1, 1, 1] => [b, h, w, c]
+    t = tf.broadcast_to(t, batch_x.shape)
+
+    interplate = t * batch_x + (1 - t) * fake_image
+
+    with tf.GradientTape() as tape:
+        tape.watch([interplate])
+        d_interplote_logits = discriminator(interplate, training=True)
+    grads = tape.gradient(d_interplote_logits, interplate)
+
+    # grads:[b, h, w, c] => [b, -1]
+    grads = tf.reshape(grads, [grads.shape[0], -1])
+    gp = tf.norm(grads, axis=1)  # [b]
+    gp = tf.reduce_mean((gp - 1) ** 2)
+
+    return gp
+
+
 def d_loss_fn(generator, discriminator, batch_z, batch_x, is_training):
     # 1. treat real image as real
     # 2. treat generated image as fake
@@ -81,9 +111,11 @@ def d_loss_fn(generator, discriminator, batch_z, batch_x, is_training):
     d_loss_real = celoss_ones(d_real_logits)  # 正样本判断为真
     d_loss_fake = celoss_zeros(d_fake_logits)  # 负样本判断为假
 
-    loss = d_loss_fake + d_loss_real  # 判别器的目标是将真的判断为真，假的判断为假
+    gp = gradient_penalty(discriminator, batch_x, fake_image)
 
-    return loss
+    loss = d_loss_fake + d_loss_real + 10. * gp  # 判别器的目标是将真的判断为真，假的判断为假，WGAN多了Gradient Penalty
+
+    return loss, gp
 
 
 def g_loss_fn(generator, discriminator, batch_z, is_training):
@@ -129,7 +161,7 @@ def main():
 
         # train D
         with tf.GradientTape() as tape:
-            d_loss = d_loss_fn(generator, discriminator, batch_z, batch_x, is_training)
+            d_loss, gp = d_loss_fn(generator, discriminator, batch_z, batch_x, is_training)
         grads = tape.gradient(d_loss, discriminator.trainable_variables)
         d_optimizer.apply_gradients(zip(grads, discriminator.trainable_variables))
 
@@ -138,12 +170,12 @@ def main():
         grads = tape.gradient(g_loss, generator.trainable_variables)
         g_optimizer.apply_gradients(zip(grads, generator.trainable_variables))
 
-        if epoch % 1000 == 0:
-            print(epoch, 'd-loss:', float(d_loss), 'g-loss:', float(g_loss))
+        if epoch % 100 == 0:
+            print(epoch, 'd-loss:', float(d_loss), 'g-loss:', float(g_loss), 'gp: ', float(gp))
 
             z = tf.random.uniform([100, z_dim])
             fake_image = generator(z, training=False)
-            img_path = '../logs/gan_images/gan-%d.png' % epoch
+            img_path = '../logs/wgan_images/wgan-%d.png' % epoch
             save_result(fake_image.numpy(), 10, img_path, color_mode='P')
 
 
